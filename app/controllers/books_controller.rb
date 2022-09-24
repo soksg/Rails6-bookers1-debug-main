47be0ece789ef821099b82281081e8fdf25b3ff9
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-24 11:45:38 +0000

    [Fix]応用課題１/editへの遷移ができないのを修正中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books=Book.all
       render :index
     end
   end
@@ -21,11 +22,13 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
